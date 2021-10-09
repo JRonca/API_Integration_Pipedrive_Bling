6a04935f406b7731b42c2f8f371759e83f0363b7
JRonca <jrfr21112000@gmail.com>
2021-10-09 20:15:24 -0300

    Create utils of services

---
+++ src/utils/blingUtils.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import jsontoxml from 'jsontoxml'
+export async function createOrder(jsonVar){
+    const api = axios.create({baseURL: process.env.BLING_BASE_URL_API})
+    const txml= encodeURIComponent('<?xml version="1.0" encoding="UTF-8"?>'+jsontoxml(jsonVar,false))
+    const result = await api.post(`/pedido/json?apikey=${process.env.BLING_KEY_API}&xml=${txml}`)
+    return result.data
+}
